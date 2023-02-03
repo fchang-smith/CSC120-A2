1e28d4cd46985bef59e9e03f30bfa4d6137f740c
Feiran Chang <fchang@smith.edu>
2023-02-03 00:26:58 -0500

    finish

--- computer.py
+++ computer.py
@@ -1,10 +1,57 @@
 class Computer:
+    item_ID: int = 0
+    description:str = ""
+    processor_type: str = ""
+    hard_drive_capacity: int = 0
+    memory: int = 0
+    operating_system: str = ""
+    year_made: int = 0
+    price: int = 0
+
+    def __init__(self, 
+                 item_ID: int = 0,
+                 description:str = "", 
+                 processor_type: str = "", 
+                 hard_drive_capacity: int = 0, 
+                 memory: int = 0, 
+                 operating_system: str = "", 
+                 year_made: int = 0, 
+                 price: int = 0) -> None:
+       self.item_ID = item_ID
+       self.description = description
+       self.processor_type = processor_type
+       self.hard_drive_capacity = hard_drive_capacity
+       self.memory = memory
+       self.operating_system = operating_system
+       self.year_made = year_made
+       self.price = price
+
+    def updatePrice(self, newPrice: int):
+        self.price = newPrice
+
+    def updateOS(self, newOS):
+        self.operating_system = newOS
+
+    def createComputer(self, computer: dict):
+        self.description = computer["description"]
+        self.processor_type = computer["processor_type"]
+        self.hard_drive_capacity = computer["hard_drive_capacity"]
+        self.memory = computer["memory"]
+        self.operating_system = computer["operating_system"]
+        self.year_made = computer["year_made"]
+        self.price = computer["price"]
+
+    def __str__(self):
+        return f'Item_ID: {self.item_ID} \n    Description: {self.description} ({self.year_made}, {self.price})'
+
+        
+
 
     # What attributes will it need?
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    #def __init__():
+    #    pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,71 @@
+import computer as cp
 class ResaleShop:
+    inventory: list = []
+    def __init__(self, inventory: list = []) -> None:
+        self.inventory = inventory
+    
+    def buy(self, newComputer: dict):
+        listLen = len(self.inventory)
+        item_ID = listLen + 1
+        computer = cp.Computer()
+        computer.createComputer(newComputer)
+        computer.item_ID = item_ID
+        self.inventory.append(computer)
+        return computer.item_ID
+        
+
+    
+    def update_price(self, item_ID: int, newPrice: int):
+        idx = item_ID - 1
+        self.inventory[idx].updatePrice(newPrice)
+
+    def sell(self, item_ID:str):
+        idx = item_ID - 1
+        listLen = len(self.inventory)
+        if idx >= 0 and idx < listLen:
+            del self.inventory[idx]
+        else:
+            print ("No such computer found")
+
+    def print_inventory(self):
+        listLen = len(self.inventory)
+        if listLen != 0:
+            for i in range(0, listLen):
+                computer = self.inventory[i]
+                print(computer)
+        else:
+            print("The inventory is empty")
+
+    def refurbish(self, item_ID: int, new_os: str = None):
+        idx = item_ID - 1
+        if idx >= 0 and idx < len(self.inventory):
+            computer = self.inventory[idx]
+            if computer.year_made < 2000:
+                computer.price = 0 
+            elif computer.year_made < 2012:
+                computer.price = 250 
+            elif computer.year_made < 2018:
+                computer.price = 550 
+            else:
+                computer.price = 1000 
+        else:
+            print("Item", item_ID, "not found. Please select another item to refurbish.")
+
+        if new_os is not None:
+            computer.operating_system = new_os
+        
+    
+
+
+
+
+        
 
     # What attributes will it need?
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+        
+         # You'll remove this when you fill out your constructor
 
     # What methods will you need?
